3537fdbc709a07d9fdccf196e31c48356822a388
Miriamvdb <miriamvdb@live.nl>
2022-07-18 11:24:27 +0200

    A lot of errors

--- src/components/DiceBoard.js
+++ src/components/DiceBoard.js
@@ -1,12 +1,47 @@
+import { useState } from "react";
 import "./DiceBoard.css";
 
 const DiceBoard = () => {
-  const randomNumber = Math.floor(Math.random() * 7);
+  const [players, setPlayers] = useState([
+    { id: 1, name: "Gonzalo", score: 0 },
+    { id: 2, name: "Miriam", score: 10 },
+  ]);
+
+  const [diceNumber, setDiceNumber] = useState(0);
+
+  const randomNumber1 = () => {
+    setDiceNumber = Math.floor(Math.random() * 7);
+  };
+
+  const randomNumber2 = Math.floor(Math.random() * 7);
+
+  const rollingDice = () => {
+    if (randomNumber1 === randomNumber2) {
+      return console.log("Tie! Roll again.");
+    } else if (randomNumber1 > randomNumber2) {
+      return console.log("Player 1 wins one point!");
+    } else {
+      return console.log("Player 2 wins one point!");
+    }
+  };
+
   return (
     <div>
-      <div>Hola Miriam {randomNumber}</div>
+      {players
+        ? players.map((player, index) => {
+            return (
+              <div key={index}>
+                <p>{player.name}</p>
+                <p>{player.score}</p>
+              </div>
+            );
+          })
+        : "Loading.."}
+      <div>{diceNumber}</div>
+      <div>{randomNumber2}</div>
       <div>
-        <button>Roll</button>
+        <button onClick={randomNumber1}>Roll</button>
+        <button onClick={randomNumber2}>Roll</button>
       </div>
     </div>
   );
